36ffe5ce1be5249da3fdc9b276aacbfb5fbe8342
dennisloliver <doliver9110@gmail.com>
2021-11-08 16:03:39 -0500

    merge conflicts

--- src/components/newForm.js
+++ src/components/newForm.js
@@ -2,11 +2,8 @@ import {useState} from 'react'
 import axios from 'axios';
 
 const NewForm = (props) => {
-<<<<<<< HEAD
-const {setLogs} = props
-=======
+
 const {setFoods} = props
->>>>>>> 815a3bd690b76bbe32fff92b082a63e01f3d2ab1
 
 const [newName, setName] = useState('')
 const [newLocation, setLocation] = useState('')
@@ -16,11 +13,9 @@ const [newPrice, setPrice] = useState('')
 const [newCurbsidePickup, setCurbsidePickup] = useState(false)
 
 const handleNewNameChange = (event) => {
-<<<<<<< HEAD
-  setNewName(event.target.value);
-=======
+
   setName(event.target.value);
->>>>>>> 815a3bd690b76bbe32fff92b082a63e01f3d2ab1
+
 }
 const handleNewLocation = (event) => {
   setLocation(event.target.value)
